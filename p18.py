5b719be59e47ab33055d58e8474c3e920eac6efc
alviria <andrea.pollitt@gmail.com>
2012-11-14 18:24:12 -0500

    commented out code in progress, working on padding the triangle into a square with zeros

--- p18.py
+++ p18.py
@@ -89,8 +89,8 @@ def pad_triangle(w,grid):
 	print(len(grid))
 	padded_grid = [0] * (w*w)
 	for i in range(int(w)):
-		w-i-1
-		padded_grid[i:i-w] = grid[i:i-w]
+#		w-i-1
+#		padded_grid[i:i-w] = grid[i:i-w]
 		print(padded_grid)
 		next_element = i+w
 	return padded_grid
@@ -98,6 +98,6 @@ def pad_triangle(w,grid):
 grid = [75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 4, 82, 47, 65, 19, 1, 23, 75, 3, 34, 88, 2, 77, 73, 7, 63, 67, 99, 65, 4, 28, 6, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
 
 w = get_width_triangle(grid)
-padded_grid = pad_triangle(w,grid)
+#padded_grid = pad_triangle(w,grid)
 
 
